1387c4afeaa78011dcc3cf19d2c1254716ff61b2
Lindo-code <sdrowvieli1@gmail.com>
2026-04-21 13:31:16 +0200

    removed testimonial side blur

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,24 +3,7 @@ import DefaultLayout from "../components/layouts/DefaultLayout";
 import "./Pages.css";
 
 const About = () => {
-  return (
-    <DefaultLayout>
-      <div className="page-container">
-        <h1>About Us</h1>
-        <p>
-          This is a React implementation of stack cards with responsive layouts.
-        </p>
-        <p>Features include:</p>
-        <ul>
-          <li>Responsive design for mobile and desktop</li>
-          <li>Smooth animations with Framer Motion</li>
-          <li>Sticky scroll effects</li>
-          <li>Intersection observer for lazy loading</li>
-          <li>CSS Grid layout system</li>
-        </ul>
-      </div>
-    </DefaultLayout>
-  );
+  return <DefaultLayout></DefaultLayout>;
 };
 
 export default About;
