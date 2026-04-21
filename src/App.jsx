e954898a35c8fb31a15001627e1bff6c927eb6d5
Lindo-code <sdrowvieli1@gmail.com>
2026-04-21 15:34:29 +0200

    added 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home_page/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
+import NotFound from "./pages/NotFound";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/projects" element={<Projects />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
